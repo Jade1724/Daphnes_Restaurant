2bd6e770652aedd630eb8aa295868560b1714533
hic21 <hic21@uclive.ac.nz>
2020-10-24 22:23:07 +1300

    Styled hamburger menu to be animated so that it will change to cross icon when the dropdown menu is shown

--- site/js/script.js
+++ site/js/script.js
@@ -1,12 +1,6 @@
-$(function () {
+$(document).ready(function () {
+  $('.hamburger-menu').on('click', function () {
 
-  $("#menuBarToggler").blur(function (event) {
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 768) {
-      $("#navbarButtons").collapse('hide');
-    }
-  });
-  $("menuBarToggler").click(function (event) {
-    $(event.target).focus();
+    $('.animated-icon').toggleClass('show');
   });
 });
